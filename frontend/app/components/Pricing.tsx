c88f84d15f773ae5248b1828eb3b2eb15e29cf97
0x00 pluto <0x00pluto@gmail.com>
2025-09-07 01:48:12 +0800

    add

--- frontend/app/components/Pricing.tsx
+++ frontend/app/components/Pricing.tsx
@@ -1,4 +1,3 @@
-import Link from 'next/link';
 import { Button } from '@/app/components/ui/Button';
 
 const Pricing = () => (
--- frontend/app/components/Tools.tsx
+++ frontend/app/components/Tools.tsx
@@ -1,5 +1,4 @@
 import Image from 'next/image';
-import Link from 'next/link';
 import ChevronRightIcon from './ChevronRightIcon';
 import { Button } from './ui/Button';
 import PageHero from './ui/PageHero';
