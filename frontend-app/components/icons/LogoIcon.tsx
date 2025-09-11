6da54506c9a76bf36a3615f9b189a31b6d4ecbcc
0x00 pluto <0x00pluto@gmail.com>
2025-09-11 11:26:49 +0800

    fix build

--- frontend-app/components/icons/LogoIcon.tsx
+++ frontend-app/components/icons/LogoIcon.tsx
@@ -1,8 +1,6 @@
 
 import React from 'react';
 
-interface LogoIconProps extends React.SVGProps<SVGSVGElement> {}
-
 const LogoIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
   <svg viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
     <path
